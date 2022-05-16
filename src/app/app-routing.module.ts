bcb3057ca12c9c88c4720d22b2b79bfa5b318a8a
xJaiki <36110302+xJaiki@users.noreply.github.com>
2022-05-16 18:18:01 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './features/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  pathMatch: "full",
+  redirectTo: "home"
+},{
+  path: "home",
+  component: HomepageComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
